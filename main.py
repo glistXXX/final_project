43e3d8c473014c1af9116628b0969504f1aad9e6
glistXXX <120323566+glistXXX@users.noreply.github.com>
2022-12-23 23:14:32 +0400

    что то

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pygame
 import sys
+import general
+import Red
+import snake
 
 pygame.init()
-widht = 500
+width = 500
 height = 500
-win = pygame.display.set_mode((widht, height))
+win = pygame.display.set_mode((width, height))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    win.fill((255, 255, 255))
+
+    for i in range(height // 30):
+        for j in range(width // 30):
+            pygame.draw.rect(win, (0, 0, 0), (j * 30, i * 30, 30, 30), 2)
+
+    def draw_block(color, ):
+        pygame.draw.rect(win, (0, 0, 0), (j * 30, i * 30, 30, 30))
+
+    pygame.display.update()
